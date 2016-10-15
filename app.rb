c912ea72ec2974c6c052c270417e8100b84142ab
Craig Iturbe <citurbe@gmail.com>
2016-10-15 14:22:08 -0400

    Create a simple text analyzer in Sinatra

--- app.rb
+++ app.rb
@@ -9,6 +9,12 @@ end
 
 post '/' do
   text_from_user = params[:user_text]
+  analyzer = TextAnalyzer.new(text_from_user)
+  @word_count = analyzer.count_of_words
+  @vowel_count = analyzer.count_of_vowels
+  @consonant_count = analyzer.count_of_consonants
+  @most_used_letter = analyzer.most_used_letter[0]
+  @letter_count = analyzer.most_used_letter[1]
   erb :results
 end
 
